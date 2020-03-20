09ca029a1bb4016d0b83b51a9483532d8a6c61fb
VINIPOOH <47758583+VINIPOOH@users.noreply.github.com>
2020-03-20 11:18:12 +0200

    decorator

---
+++ GofPaterns/src/decorator/Printer.java
@@ -0,0 +1,16 @@
+package composite;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public interface Printer {
+    void print();
+    default void add(Printer printer){
+        throw new NotImplementedException();
+    }
+    default void remove(Printer printer){
+        throw new NotImplementedException();
+    }
+    default Printer getChild(int index){
+        throw new NotImplementedException();
+    }
+}
