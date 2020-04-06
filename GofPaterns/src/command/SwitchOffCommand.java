dbdc72ff4667acee5a6cc788d9ae865dd2b02bc0
VINIPOOH <47758583+VINIPOOH@users.noreply.github.com>
2020-04-06 16:42:27 +0300

    command

---
+++ GofPaterns/src/command/SwitchOffCommand.java
@@ -0,0 +1,15 @@
+package command;
+
+/** The Command for turning off the light - ConcreteCommand #2 */
+class SwitchOffCommand implements Command {
+    private final Light light;
+
+    public SwitchOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override // Command
+    public void execute() {
+        light.turnOff();
+    }
+}
